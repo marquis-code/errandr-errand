0ed96787e9385e59c1a2efbed8b37bab12711a06
Marquis <abahmarquis@gmail.com>
2026-05-19 16:08:20 +0100

    updated

--- .output/public/_nuxt/CVB_WQ0D.js
+++
@@ -1,1 +0,0 @@
-import{an as e}from"./DLK95QHk.js";const g=e({defaults:{"@type":"AggregateRating"}});export{g as aggregateRatingResolver};
--- .output/public/_nuxt/DoN9OU1H.js
+++
@@ -1,1 +0,0 @@
-import"./DLK95QHk.js";const r=""+new URL("logo.7u_gNOaX.png",import.meta.url).href;export{r as _};
